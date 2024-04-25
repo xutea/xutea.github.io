27443518c8d2b08a9130fb37dcd631cdc9b96d6b
liujing <jing.liu@cestong.com>
2024-04-25 18:20:46 +0800

    提交源代码

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from './router'
-// import 'lib-flexible/flexible'
-createApp(App).use(router).mount('#app')
-// 使用路由器
-
